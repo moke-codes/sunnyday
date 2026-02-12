949c2bc60afde6a939f952a7fc03373882490f38
Leandro Gonçalves <lesigo@gmail.com>
2026-02-12 04:48:27 +0000

    Port 5000 as default

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
